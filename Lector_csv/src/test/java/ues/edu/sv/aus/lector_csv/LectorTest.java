f7bbe6ae84fee27e746c63aff3c1d70dd6e72018
Juan Ramos <Juan Ramos@DESKTOP-LCCCD9E>
2019-04-12 17:32:41 -0600

    agrego2

--- Lector_csv/src/test/java/ues/edu/sv/aus/lector_csv/LectorTest.java
+++ Lector_csv/src/test/java/ues/edu/sv/aus/lector_csv/LectorTest.java
@@ -92,7 +92,7 @@ public class LectorTest {
         boolean result = instance.verificarArchivo(path);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
     }
 
     /**
